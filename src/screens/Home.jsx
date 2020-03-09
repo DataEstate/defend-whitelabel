55b64b45566488397fbe2cacae4faa6f91bd14dc
YouHao <haoyou0807@163.com>
2020-03-09 15:05:41 +1000

    update button style with  Material-UI's makeStyles hook

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -4,21 +4,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-export const Home = () => (
-  <div>
-    <Typography variant='h3'>{'This is Home page'}</Typography>
-    <Link to='/listview'>
-      <Button
-        variant='contained'
-        color='primary'
-        href='#contained-buttons'
-        style={{ marginBottom: 10 }}
-      >
-        Link to ListView
-      </Button>
-    </Link>
-  </div>
-);
+const useStyles = makeStyles({
+  button: {
+    marginBottom: 10
+  }
+});
+export const Home = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <Typography variant='h3'>{'This is Home page'}</Typography>
+      <Link to='/listview'>
+        <Button
+          variant='contained'
+          color='primary'
+          href='#contained-buttons'
+          className={classes.button}
+        >
+          Link to ListView
+        </Button>
+      </Link>
+    </div>
+  );
+};
 
 export default Home;
--- src/screens/ListView.jsx
+++ src/screens/ListView.jsx
@@ -2,19 +2,30 @@ import React from 'react';
 import { Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-export const ListView = () => (
-  <div>
-    <Typography variant='h3'>{'This is ListView page'}</Typography>
-    <Link to='/'>
-      <Button
-        variant='contained'
-        color='primary'
-        href='#contained-buttons'
-        style={{ marginBottom: 10 }}
-      >
-        Back to Home Page
-      </Button>
-    </Link>
-  </div>
-);
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  button: {
+    marginBottom: 10
+  }
+});
+export const ListView = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Typography variant='h3'>{'This is ListView page'}</Typography>
+      <Link to='/'>
+        <Button
+          variant='contained'
+          color='primary'
+          href='#contained-buttons'
+          className={classes.button}
+        >
+          Back to Home Page
+        </Button>
+      </Link>
+    </div>
+  );
+};
 export default ListView;

70f69eec63e81f77641492cd7993b5d392ac29cb
YouHao <haoyou0807@163.com>
2020-03-19 22:34:42 +1000

    ListView Test update

--- src/screens/__tests__/ListView.spec.js
+++ src/screens/__tests__/ListView.spec.js
@@ -2,19 +2,14 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import { ListView } from '../ListView';
-import { Typography } from '@material-ui/core';
+import { Typography, Button } from '@material-ui/core';
 
 describe('src/screens/ListView', () => {
   it('jump to other pages with Link', () => {
     const fakeComponent = shallow(<ListView />);
 
-    console.log(fakeComponent.debug());
-    expect(fakeComponent.find('.makeStyles-button-1').text()).to.equal(
-      'Back to Home Page'
-    );
+    expect(fakeComponent.find(Button).prop('variant')).to.equal('contained');
 
-    expect(fakeComponent.find(Typography).text()).to.equal(
-      'This is ListView page'
-    );
+    expect(fakeComponent.find(Typography).text()).to.equal('This is ListView page');
   });
 });

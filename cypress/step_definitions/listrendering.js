0dc358f4b598e47d131ef3f440dae8d21e86700c
Anova Fawzi <anovafawzi@gmail.com>
2020-03-26 16:45:23 +0700

    update cypress test with device type parameter

--- cypress/step_definitions/listrendering.js
+++ cypress/step_definitions/listrendering.js
@@ -1,71 +1,62 @@
 /* global Given, When, Then, cy */
 
-Given(/I see that there are "([^"]*)" listings on one row on desktop/, screen => {
+Given(/I see that there are "([^"]*)" listings on one row on (desktop|tablet|mobile)/, (screen, device) => {
+  const viewport = device === 'desktop' 
+                    ? 'macbook-13'
+                    : device === 'tablet'
+                      ? 'ipad-2'
+                      : 'iphone-x'; 
   // desktop screen
-  cy.viewport('macbook-13');
-
-  // get the first listing
-  cy.get('[data-test-id="estate-item-0"]').then($item1 => {
-    let coordsCompare1 = $item1[0].getBoundingClientRect();
-    let coordsCompare2 = null;
-    
-    // compare it with fourth listing
-    cy.get('[data-test-id="estate-item-3"]').then($item4 => {
-      coordsCompare2 = $item4[0].getBoundingClientRect();
-
-      expect(coordsCompare1.y).to.equal(coordsCompare2.y);
-    });
-
-    // compare it with fifth listing
-    cy.get('[data-test-id="estate-item-4"]').then($item5 => {
-      coordsCompare2 = $item5[0].getBoundingClientRect();
-
-      expect(coordsCompare1.y).to.not.equal(coordsCompare2.y);
-    });
-  
-  });
-});
-
-Given(/I see that there are "([^"]*)" listings on one row on tablet/, screen => {
-  // desktop screen
-  cy.viewport('ipad-2');
+  cy.viewport(viewport);
 
   // get the first listing
   cy.get('[data-test-id="estate-item-0"]').then($item1 => {
     let coordsCompare1 = $item1[0].getBoundingClientRect();
     let coordsCompare2 = null;
 
-    // compare it with third listing
-    cy.get('[data-test-id="estate-item-2"]').then($item3 => {
-      coordsCompare2 = $item3[0].getBoundingClientRect();
+    // if desktop
+    if (device === 'desktop') {
+      // compare it with fourth listing
+      cy.get('[data-test-id="estate-item-3"]').then($item4 => {
+        coordsCompare2 = $item4[0].getBoundingClientRect();
 
-      expect(coordsCompare1.y).to.equal(coordsCompare2.y);
-    });
+        expect(coordsCompare1.y).to.equal(coordsCompare2.y);
+      });
 
-    // compare it with fourth listing
-    cy.get('[data-test-id="estate-item-3"]').then($item4 => {
-      coordsCompare2 = $item4[0].getBoundingClientRect();
+      // compare it with fifth listing
+      cy.get('[data-test-id="estate-item-4"]').then($item5 => {
+        coordsCompare2 = $item5[0].getBoundingClientRect();
 
-      expect(coordsCompare1.y).to.not.equal(coordsCompare2.y);
-    });
-  
-  });
-});
+        expect(coordsCompare1.y).to.not.equal(coordsCompare2.y);
+      });
+    }
 
-Given(/I see that there are "([^"]*)" listings on one row on mobile/, screen => {
-  // desktop screen
-  cy.viewport('iphone-x');
+    // if tablet
+    if (device === 'tablet') {
+      // compare it with third listing
+      cy.get('[data-test-id="estate-item-2"]').then($item3 => {
+        coordsCompare2 = $item3[0].getBoundingClientRect();
 
-  // get the first listing
-  cy.get('[data-test-id="estate-item-0"]').then($item1 => {
-    let coordsCompare1 = $item1[0].getBoundingClientRect();
+        expect(coordsCompare1.y).to.equal(coordsCompare2.y);
+      });
 
-    // get the second listing
-    cy.get('[data-test-id="estate-item-1"]').then($item2 => {
-      let coordsCompare2 = $item2[0].getBoundingClientRect();
+      // compare it with fourth listing
+      cy.get('[data-test-id="estate-item-3"]').then($item4 => {
+        coordsCompare2 = $item4[0].getBoundingClientRect();
 
-      expect(coordsCompare1.y).to.not.equal(coordsCompare2.y);
-    });
-  
+        expect(coordsCompare1.y).to.not.equal(coordsCompare2.y);
+      });
+    }
+
+    // if mobile
+    if (device === 'mobile') {
+      // compare with second listing
+      cy.get('[data-test-id="estate-item-1"]').then($item2 => {
+        let coordsCompare2 = $item2[0].getBoundingClientRect();
+
+        expect(coordsCompare1.y).to.not.equal(coordsCompare2.y);
+      });
+    }
+      
   });
 });

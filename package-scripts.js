7645dc092ccd9fd2183188e761cc306ec06a3302
Rolf Chen <rolf.chen@dataestate.com.au>
2020-03-29 18:35:39 +1000

    Added npm-run-all, updated steps for running cypress test.

--- package-scripts.js
+++ package-scripts.js
@@ -6,6 +6,11 @@ module.exports = {
     flow: "flow",
     local:
       "cross-env NODE_ENV=local webpack-dev-server --open --mode development",
+    codeintegrity: series(
+      "npx nps flow",
+      "npx nps test",
+      "npx run-p -r start cytest"
+    ),
     test: "npx jest",
     flow: "npx flow check",
     cytest: "npx cypress run"

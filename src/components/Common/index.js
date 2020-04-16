0c92466edb10cbe3a09efbeb523b89413ae7e56c
Anova Fawzi <anovafawzi@gmail.com>
2020-04-17 05:49:07 +0700

    use StarRatingIcons component for EstateCardStarRatings

--- src/components/Common/index.js
+++ src/components/Common/index.js
@@ -1,1 +1,2 @@
 export { default as EmptyImagePlaceholder } from "./EmptyImagePlaceholder";
+export { default as StarRatingIcons } from "./StarRatingIcons";
--- src/components/Estates/EstateCardStarRatings.jsx
+++ src/components/Estates/EstateCardStarRatings.jsx
@@ -3,9 +3,7 @@
 import * as React from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
-import StarIcon from '@material-ui/icons/Star';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
-import StarHalfIcon from '@material-ui/icons/StarHalf';
+import { StarRatingIcons } from "src/components/Common";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,13 +21,9 @@ const useStyles = makeStyles((theme) => ({
   },
   starSection: {
     color: "#FABA01",
-    marginTop: -8,
-    marginBottom: 11,
-  },
-  starIcon: {
     height: 14,
     width: 14,
-  }
+  },
 }));
 
 type Props = {
@@ -38,42 +32,11 @@ type Props = {
 
 export const EstateCardStarRatings = ({ rating }: Props) => {
   const classes = useStyles();
-
-  const generateStarsIcon = () => {
-    if (rating < 0 || rating > 5) {
-      return null;
-    } else {
-      const remainder = 5 - rating;
-      
-      const emptyStar = remainder < 1 ? 0 : parseInt(remainder);
-      const halfStar = (rating % 1) / 0.5;
-      const fullStar = parseInt(rating);
-
-      let starRating = [];
-      if (fullStar > 0) {
-        for (let index = 0; index < fullStar; index++) {
-          starRating.push(<StarIcon className={classes.starIcon} />);
-        }
-      }
-      if (halfStar > 0) {
-        for (let index = 0; index < halfStar; index++) {
-          starRating.push(<StarHalfIcon className={classes.starIcon} />);
-        }
-      }
-      if (emptyStar > 0) {
-        for (let index = 0; index < emptyStar; index++) {
-          starRating.push(<StarBorderIcon className={classes.starIcon} />);
-        }
-      }
-
-      return starRating;
-    }
-  }
   
   return (
     <div className={classes.root}>
       <Typography variant={"h2"} className={classes.title}>Star Rating</Typography>
-      <Typography variant={"h6"} className={classes.starSection}>{generateStarsIcon()}</Typography>
+      <StarRatingIcons className={classes.starSection} rating={rating} />
     </div>
   );
 };

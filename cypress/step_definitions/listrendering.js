29d30695b922af799bef0ef7ed80544c612f56ff
Anova Fawzi <anovafawzi@gmail.com>
2020-05-01 09:36:56 +0700

    initial code for cypress test of the estate listing filter

--- cypress/step_definitions/listrendering.js
+++ cypress/step_definitions/listrendering.js
@@ -7,8 +7,8 @@ Given(
       device === "desktop"
         ? "macbook-13"
         : device === "tablet"
-        ? "ipad-2"
-        : "iphone-x";
+          ? "ipad-2"
+          : "iphone-x";
     // desktop screen
     cy.viewport(viewport);
 
@@ -86,4 +86,80 @@ Given(/I see that there are "([^"]*)" listings from API/, (totalData) => {
   cy.get("#EstateListing")
     .find('[data-test-id*="estate-item"]')
     .should("have.length", totalData);
+});
+
+Given(/I see filter with default placeholder text "([^"]*)" and there are selected data with these categories "([^"]*)" from API/, (filterPlaceholder, categories) => {
+  // get EstateCards
+  cy.get(`#EstateListing`).should("exist");
+
+  // we assume for the filter text to be exists
+  cy.get("#ListViewScreen")
+    .find('input[name="estate-filter"]')
+    .invoke('attr', 'placeholder')
+    .should("contain", filterPlaceholder);
+
+  // check the xhr
+  cy.wait('@getEstate').then((xhr) => {
+    expect(xhr.method).to.equal('GET');
+    const url = new URL(xhr.url);
+    const categoriesParam = url.searchParams.get('categories');
+    expect(categoriesParam).to.equal(categories);
+  });
+
+});
+
+Given(/I select filter "([^"]*)" and I see text "([^"]*)" and there are selected data with these categories "([^"]*)" from API/, (selectedFilter, filterPlaceholder, categories) => {
+  // get EstateCards
+  cy.get(`#EstateListing`).should("exist");
+
+  // click on filter
+  cy.get("#ListViewScreen")
+    .find('[data-filter-name="estate-filter"]')
+    .click();
+
+  // check if multiple
+  const filters = selectedFilter.split(',');
+
+  if (filters.length === 1) {
+    // if only one filter
+    // select the li class
+    cy.get('li.MuiListItem-button').contains(selectedFilter).then(option => {
+      // confirm have correct option
+      cy.wrap(option).contains(selectedFilter);
+
+      // click on that option
+      option[0].click();
+
+      // after click, select input text must contains that selected label
+      cy.get('#mui-component-select-estate-filter').contains(filterPlaceholder);
+    });
+
+  } else if (filters.length > 1) {
+    // if multiple
+    for (let i = 0; i < filters.length; i++) {
+      // select the li class
+      cy.get('li.MuiListItem-button').contains(filters[i]).then(option => {
+        // confirm have correct option
+        cy.wrap(option).contains(filters[i]);
+
+        // click on that option
+        option[0].click();
+      });
+    }
+
+    // after click, select input text must contains that selected label
+    cy.get('#mui-component-select-estate-filter').contains(filterPlaceholder);
+  }
+
+  // check the xhr for the correct parameters
+  cy.server();
+  cy.route('GET', '/v2/estates/*').as('getNextEstate');
+
+  cy.wait('@getNextEstate').then((xhr) => {
+    expect(xhr.method).to.equal('GET');
+    const url = new URL(xhr.url);
+    const categoriesParam = url.searchParams.get('categories');
+    expect(categoriesParam).to.equal(categories);
+  });
+
 });
